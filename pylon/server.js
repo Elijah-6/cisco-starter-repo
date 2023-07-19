2bea017f98ee6f1a6f55ad6b502e557ce6f2dc11
Elijah-6 <elijahfrimpong76@gmail.com>
2023-07-19 23:15:57 +0000

    create react base

--- pylon/server.js
+++ pylon/server.js
@@ -1,5 +1,5 @@
 const webSocketServer = require('websocket').server;
-const http = require('http');
+const http= require('http');
 
 const server = http.createServer();
 server.listen(55455);
@@ -7,7 +7,7 @@ const wsServer = new webSocketServer({ httpServer: server });
 
 wsServer.on('request', function (request) {
     console.log('establishing a new connection with client');
-    var connection = request.accept(null, request.origin);
+    const connection = request.accept(null, request.origin);
     setInterval(() => {
         connection.sendUTF(new Date().getTime())
     }, 100);
---
+++ sextant/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className='fl'>
+      <div className='hello'>
+        Hello
+      </div>
+      <h1>Hello Sextant</h1>
+      <code>Your name</code>
+      </div>
+    </div>
+  );
+}
+
+export default App;
